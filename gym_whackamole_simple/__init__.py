69be278505074a4bb9c91f3bf77e3d83ee79305d
Siyu Wang <wangxsiyu@gmail.com>
2022-07-27 19:46:06 -0400

    init

--- gym_whackamole_simple/__init__.py
+++ gym_whackamole_simple/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='gym_whackamole/WhackAMole-v0',
+    id='gym_whackamole_simple/WhackAMole-v0',
     entry_point='gym_whackamole_simple.envs:WhackAMole',
     max_episode_steps = None,
 )
