171b4ff44e4bb6d074a6688d8e6cb43b530372fb
Siyu Wang <wangxsiyu@gmail.com>
2022-07-27 20:24:24 -0400

    Update __init__.py

--- gym_whackamole_simple/__init__.py
+++ gym_whackamole_simple/__init__.py
@@ -1,10 +1,10 @@
 from gym.envs.registration import register
-import gym
-import gym.envs.registry
+# import gym
+# import gym.envs.registry
 
-env_name = 'gym_whackamole_simple/WhackAMole-v0'
-if env_name in gym.envs.registry.env_specs:
-    del gym.envs.registry.env_specs[env_name]
+# env_name = 'gym_whackamole_simple/WhackAMole-v0'
+# if env_name in gym.envs.registry.env_specs:
+#     del gym.envs.registry.env_specs[env_name]
 
 register(
     id='gym_whackamole_simple/WhackAMole-v0',
