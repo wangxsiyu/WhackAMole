3c6bce641e2e58ff3824479a711db7c99f4a000c
Siyu Wang <wangxsiyu@gmail.com>
2022-07-29 14:50:26 -0400

    4moles

--- gym_lookamole/envs/mole.py
+++ gym_lookamole/envs/mole.py
@@ -10,21 +10,29 @@ class Mole(spaces.Box):
         self.reset()
 
     def step(self): 
-        if np.random.random() < self.params['p_switch']:
-            self.pop()
+        self.params['p_popping'] = 0.1
+        if self.mole_life > 0:
+            if np.random.random() < self.params['p_countdown']:
+                self.mole_life -= 1
+        elif self.mole_life == 0:
+            if np.random.random() < self.params['p_popping']:
+                self.reset()
+            else:
+                self._mole_location = np.array((0,0))
 
     def obs(self):
         return {"xy": self._mole_location}
     
     def reset(self):
         ps = self.params['locations']
-        i = 0 #np.random.randint(len(ps))
+        self.mole_life = 20
+        i = np.random.randint(len(ps))
         self._mole_location = np.array(ps[i])
 
     def set_task_parameters(self, params = None):
         if params is None:
             params = dict()
-            params['p_switch'] = 0
+            params['p_countdown'] = 0.9
             params['radius'] = 30
             wh = np.array(self.window_size)
             params['locations'] = np.array([[0.1, 0.1] * wh,[0.1, 0.9] * wh,[0.9, 0.1] * wh,[0.9, 0.9] * wh])
@@ -34,9 +42,10 @@ class Mole(spaces.Box):
         return self.params
 
     def _render_frame(self, canvas):
-        pygame.draw.circle(
-            canvas,
-            (0, 255, 0),
-            self._mole_location,
-            self.params["radius"],
-        )
+        if all(self._mole_location != np.array((0,0))):
+            pygame.draw.circle(
+                canvas,
+                (0, 255, 0),
+                self._mole_location,
+                self.params["radius"],
+            )
--- main_train.py
+++ main_train.py
@@ -1,22 +1,26 @@
-from gym_lookamole.envs import LookAMole
+import gym_lookamole.envs.lookamole as lam
 from visualize import visualize
 from evaluate import eval
 from dqn.dqn import DQN
 import torch
+import importlib
 
-env = LookAMole(render_mode=None, render_fps = 20, n_frame_per_episode = 100)
+importlib.reload(lam)
 
-
+env = lam.LookAMole(render_mode=None, render_fps = 20, n_frame_per_episode = 100)
+# params = env.params
+# params['mole']['p_countdown'] = 0.9
+# env.set_params(params)
 
 # dqn = DQN(env)
 dqn = torch.load('DQN_trained')
-# dqn.train(1000, n_log = 100)
-# torch.save(dqn,'DQN_trained')
+dqn.train(10000, n_log = 100)
+torch.save(dqn,'DQN_trained')
 # eval(env, dqn)
 
 is_record = False
 rmode = "rgb_array" if is_record else "human"
-venv = LookAMole(render_mode=rmode, render_fps = 20)
+venv = lam.LookAMole(render_mode=rmode, render_fps = 20)
 visualize(venv, dqn, is_record = is_record)
 
 
